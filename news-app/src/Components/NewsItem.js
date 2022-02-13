f3af5b7305287df65330240cb4c56c74d2de6564
agrawal-parakh <parakh7055@gmail.com>
2022-02-13 15:30:55 +0530

    using loop newsitem added

--- news-app/src/Components/NewsItem.js
+++ news-app/src/Components/NewsItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
 export default class NewsItem extends Component {
+
     render() {
-        let { title, desc } = this.props;
+        let { title, desc, imageurl,url } = this.props;
         return (
             <>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img className="card-img-top" src="https://www.theglobeandmail.com/resizer/qcNSOTfTYx48PInsSBs-ZLkYetk=/1200x800/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/tgam/R2HCGH6HSRF6DB3M4SA4YSTYQ4.JPG" alt="Card image cap" />
+                    <img className="card-img-top" src={imageurl} alt="" />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{desc}</p>
-                        <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                        <a href={url} target="_blank"  className="btn btn-primary">Read More</a>
                     </div>
                 </div>
             </>
